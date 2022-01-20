728a8ff0d35abdc9d3dca2dd406127eeeb5bb7ef
aDron1990 <andreysheveliov1990@gmail.com>
2022-01-20 16:07:05 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace VigenerCoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.word_textBox = new System.Windows.Forms.TextBox();
+            this.result_textBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.key_textBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // word_textBox
+            // 
+            this.word_textBox.Location = new System.Drawing.Point(30, 60);
+            this.word_textBox.Name = "word_textBox";
+            this.word_textBox.Size = new System.Drawing.Size(334, 24);
+            this.word_textBox.TabIndex = 0;
+            this.word_textBox.Text = "word";
+            this.word_textBox.TextChanged += new System.EventHandler(this.word_textBox_TextChanged);
+            // 
+            // result_textBox
+            // 
+            this.result_textBox.Location = new System.Drawing.Point(30, 274);
+            this.result_textBox.Name = "result_textBox";
+            this.result_textBox.Size = new System.Drawing.Size(334, 24);
+            this.result_textBox.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(30, 146);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(154, 96);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Action";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 60);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 22);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Decrypt";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(79, 22);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Encrypt";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // key_textBox
+            // 
+            this.key_textBox.Location = new System.Drawing.Point(30, 102);
+            this.key_textBox.Name = "key_textBox";
+            this.key_textBox.Size = new System.Drawing.Size(334, 24);
+            this.key_textBox.TabIndex = 3;
+            this.key_textBox.Text = "key";
+            this.key_textBox.TextChanged += new System.EventHandler(this.key_textBox_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Cyrillic",
+            "Latin"});
+            this.comboBox1.Location = new System.Drawing.Point(210, 155);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(154, 26);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(399, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(75, 26);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 335);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.key_textBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.result_textBox);
+            this.Controls.Add(this.word_textBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Vigener coder";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox word_textBox;
+        private System.Windows.Forms.TextBox result_textBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox key_textBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VigenerCoder
+{
+    public partial class Form1 : Form
+    {
+        private bool mode = true;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 1;
+            updateResult();
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            mode = radioButton1.Checked;
+            updateResult();
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            mode = radioButton1.Checked;
+            updateResult();
+        } 
+        private void word_textBox_TextChanged(object sender, EventArgs e)
+        {
+            updateResult();
+        }
+        private void key_textBox_TextChanged(object sender, EventArgs e)
+        {
+            updateResult();
+        }
+
+        private void updateResult()
+        {
+            if (word_textBox.Text == "" || key_textBox.Text == "")
+            {
+                result_textBox.Text = null;
+                return;
+            }
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    result_textBox.Text = crypt(word_textBox.Text, key_textBox.Text, mode, 'а', 'я');
+                    break;
+                case 1:
+                    result_textBox.Text = crypt(word_textBox.Text, key_textBox.Text, mode, 'a', 'z');
+                    break;
+            }
+            
+        }
+        private string crypt(string word, string key, bool mode, char fAlphSym, char lAlphSym)
+        {
+            word = word.ToLower();
+            key = key.ToLower();
+
+            string result = null;
+
+            int[] dWord = new int[word.Length];
+            int[] dKey = new int[key.Length];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char temp = word[i];
+                if ((int)temp >= (int)fAlphSym && (int)temp <= (int)lAlphSym)
+                {
+                    temp = mode ? (char)((int)temp + ((int)key[(i) % (key.Length)] - fAlphSym + 1))
+                        : (char)((int)temp - ((int)key[(i) % (key.Length)] - fAlphSym + 1));
+
+                    if ((int)temp < (int)fAlphSym) temp = (char)(lAlphSym - (fAlphSym - temp - 1));
+                    else if ((int)temp > (int)lAlphSym) temp = (char)(fAlphSym + (temp - lAlphSym - 1));
+
+                }
+
+                result = result + temp;
+            }
+
+            return result;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            updateResult();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Vigener coder v0.1\nby aDron", "About", MessageBoxButtons.OK);
+        }
+    }
+}
